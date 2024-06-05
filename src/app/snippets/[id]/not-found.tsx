6af97e412add873c0cb56a039bf23fa0521c91fc
Chethan <chethanprakash720@gmail.com>
2024-06-05 20:05:19 +0530

    Delete src directory

--- src/app/snippets/[id]/not-found.tsx
+++
@@ -1,8 +0,0 @@
-export default function notFoundSnippet(){
-    return(
-        <h1 className="text-xl bold">
-            Sorry, but we couldnt find that particular snippet 
-        </h1>
-
-    )
-}
