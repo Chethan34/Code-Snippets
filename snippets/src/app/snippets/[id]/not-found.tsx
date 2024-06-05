e28c4eaea2fa7e2a5d4c43378ca747bfc67055e0
Chethan <chethanprakash720@gmail.com>
2024-06-05 19:49:12 +0530

    added save,edit,delete options

---
+++ snippets/src/app/snippets/[id]/not-found.tsx
@@ -0,0 +1,8 @@
+export default function notFoundSnippet(){
+    return(
+        <h1 className="text-xl bold">
+            Sorry, but we couldnt find that particular snippet 
+        </h1>
+
+    )
+}
