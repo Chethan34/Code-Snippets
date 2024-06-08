f116591f807a6cc929d7e7c5447cbdc2c8a5d5a7
TazzMa <chethanprakash720@gmail.com>
2024-06-08 20:40:48 +0530

    added final caching systems
    Improved Performace using different caching systems

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -2,13 +2,14 @@
 
 import { redirect } from 'next/navigation';
 import { db } from '@/db';
+import { revalidatePath } from 'next/cache';
 
 export async function editSnippet(id: number, code: string) {
   await db.snippet.update({
     where: { id },
     data: { code },
   });
-
+  revalidatePath(`/snippets/${id}`)
   redirect(`/snippets/${id}`);
 }
 
@@ -16,7 +17,7 @@ export async function deleteSnippet(id: number) {
   await db.snippet.delete({
     where: { id },
   });
-
+  revalidatePath('/');
   redirect('/');
 }
 
@@ -58,7 +59,7 @@ if(err instanceof Error){
         message:'Something went wrong..'
     };
 }
-}
+}   revalidatePath('/');
     // Redirect the user back to the root route
     redirect('/');
   };
